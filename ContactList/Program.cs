5d813c6ba8efc1008423b51296a08ea4ee00f40a
Brandon Zega <brandon_zega@yahoo.com>
2021-11-03 14:25:05 -0400

    first commit

---
+++ ContactList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContactList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
